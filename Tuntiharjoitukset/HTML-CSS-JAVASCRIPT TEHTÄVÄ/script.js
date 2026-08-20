f58ca98b7c91784813e467b76a78dfd56d6cb450
smkivela <sm.kivela@gmail.com>
2026-08-20 08:55:58 +0300

    aloitettu lopputehtävää

---
+++ Tuntiharjoitukset/HTML-CSS-JAVASCRIPT TEHTÄVÄ/script.js
@@ -0,0 +1,15 @@
+
+
+const lomake = document.getElementById("Lomake")
+
+console.log(lomake);
+
+
+// LUODAAN VÄHÄN NIINKUIN LAMPULLE OMA JS ID ELI CONST //
+
+const lamppu = document.getElementById("lamppu")
+
+lamppu.addEventListener("click", function() {
+lamppu.src = "kuvat/lamppusyt.png"
+});
+
